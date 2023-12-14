1a319f8db593acbb8b4780af02f06955ec0302a7
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-14 12:14:05 +0100

    ADD last exercise

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+/*
+Consegna
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+/*
+Consegna
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html,
+e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba,
+perciò nell'array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
+*/
+
+
+//funzione per far creare la griglia
+function play() {
+
+    //dichiaro una costante sulla selezione di difficoltà
+    const select = document.getElementById('difficulty');
+
+    //dichiaro due variabili in base a numero e difficoltà
+    let cellsNumber;
+    let difficultyClass;
+
+    //in base alla difficoltà assegno un numero di celle e do una classe che su css indicherà la larghezza della cella
+    if (select.value === 'easy') {
+        cellsNumber = 49;
+        difficultyClass = 'easy';
+    } else if (select.value === 'medium') {
+        cellsNumber = 81;
+        difficultyClass = 'medium';
+    } else if (select.value === 'hard') {
+        cellsNumber = 100;
+        difficultyClass = 'hard';
+    }
+
+    //se ci sono già celle esco dalla funzione per non creare altre griglie
+    if (document.querySelectorAll('.cell').length > 0) {
+        return;
+    }
+
+    //richiamo la funzione
+    generateCells(cellsNumber, difficultyClass);
+
+}
+
+//creo una funzione per generare le celle
+function generateCells(cellsNumber, difficulty) {
+
+    //dichiaro una costante
+    const grid = document.getElementById('grid');
+
+    //faccio un ciclo in base al numero di celle dovuto alla difficoltà selezionata
+    for (let i = 1; i <= cellsNumber; i++) {
+
+        //creo un div per ogni ciclo
+        const cell = document.createElement('div');
+
+        //inserico il numero progressivo nella cella 
+        cell.innerHTML = i;
+
+        //aggiunge la variabile difficulty alla cella
+        cell.classList.add('cell', difficulty);
+
+        //inserisco la cella nella griglia
+        grid.appendChild(cell);
+
+        //al click sulla cella parte la funzione
+        cell.addEventListener('click', function() {
+                
+        //ogni cella selezionata verrà aggiunta la classe active, se già selezionata verrà tolta
+        this.classList.toggle('active');
+                        
+        //stampo in console il numero della cella
+        console.log('Hai cliccato sulla cella N° ' + this.innerHTML);
+                    
+        });
+
+    }
+}
+
+//funzione per il tasto reset che svuoterà il contenitore della griglia
+function reset() {
+    grid.innerHTML = '';
+}
